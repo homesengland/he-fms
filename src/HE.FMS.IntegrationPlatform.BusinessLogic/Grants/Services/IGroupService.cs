073cda99dd42aeb879a3148f2d0c8916b6ab1b49
Jakub Jankowski <jakub.jankowski1987@gmail.com>
2024-08-01 08:36:46 +0200

    AB#101037 Introduce Durable function for crating Grant account orchestration.

--- src/HE.FMS.IntegrationPlatform.BusinessLogic/Grants/Services/IGroupService.cs
+++ src/HE.FMS.IntegrationPlatform.BusinessLogic/Grants/Services/IGroupService.cs
@@ -3,7 +3,7 @@ using HE.FMS.IntegrationPlatform.Providers.Mambu.Api.Group.Contract;
 
 namespace HE.FMS.IntegrationPlatform.BusinessLogic.Grants.Services;
 
-internal interface IGroupService
+public interface IGroupService
 {
     Task<GroupReadDto> GetOrCreateGroup(OrganisationContract organisation, CancellationToken cancellationToken);
 }
---
+++ src/HE.FMS.IntegrationPlatform/Functions/Activities/Grants/OpenNewGrantAccount/CreateCreditArrangement.cs
@@ -0,0 +1,29 @@
+﻿using HE.FMS.IntegrationPlatform.BusinessLogic.Grants.Services;
+using HE.FMS.IntegrationPlatform.Contract.Grants.UseCases;
+using HE.FMS.IntegrationPlatform.Providers.Mambu.Api.CreditArrangement.Contract;
+using HE.FMS.IntegrationPlatform.Providers.Mambu.Api.Group.Contract;
+using Microsoft.DurableTask;
+
+namespace HE.FMS.IntegrationPlatform.Functions.Activities.Grants.OpenNewGrantAccount;
+
+[DurableTask(nameof(CreateCreditArrangement))]
+public class CreateCreditArrangement : TaskActivity<CreateCreditArrangement.CreateCreditArrangementInput, CreditArrangementReadDto>
+{
+    private readonly ICreditArrangementService _creditArrangementService;
+
+    public CreateCreditArrangement(ICreditArrangementService creditArrangementService)
+    {
+        _creditArrangementService = creditArrangementService;
+    }
+
+    public override async Task<CreditArrangementReadDto> RunAsync(TaskActivityContext context, CreateCreditArrangementInput input)
+    {
+        return await _creditArrangementService.GetOrCreateCreditArrangement(
+            input.Request.ApplicationId,
+            input.Group.EncodedKey,
+            input.Request.GrantDetails,
+            CancellationToken.None);
+    }
+
+    public record CreateCreditArrangementInput(OpenNewGrantAccountRequest Request, GroupReadDto Group);
+}
