01096b04eb4e4d046e4b7e42510848e25241a616
Marcin Gradzki <marcin.gradzki@pwc.com>
2024-08-07 16:52:16 +0200

    AB#100921 Validate open new grant account request

--- src/HE.FMS.Middleware.Common/Config/CommonModule.cs
+++ src/HE.FMS.Middleware.Common/Config/CommonModule.cs
@@ -1,12 +1,21 @@
-﻿using HE.FMS.Middleware.Common.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using HE.FMS.Middleware.Common.Serialization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HE.FMS.Middleware.Common.Config;
 
 public static class CommonModule
 {
+    public static JsonSerializerOptions CommonSerializerOptions => new()
+    {
+        PropertyNameCaseInsensitive = true,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
+    };
+
     public static IServiceCollection AddCommonModule(this IServiceCollection services)
     {
+        services.AddSingleton(CommonSerializerOptions);
         services.AddSingleton<IStreamSerializer, StreamSerializer>();
 
         return services;
--- src/HE.FMS.Middleware.Common/Serialization/IStreamSerializer.cs
+++ src/HE.FMS.Middleware.Common/Serialization/IStreamSerializer.cs
@@ -1,6 +1,10 @@
+using System.Text.Json;
+
 namespace HE.FMS.Middleware.Common.Serialization;
 
 public interface IStreamSerializer
 {
     Task<T> Deserialize<T>(Stream stream, CancellationToken cancellationToken);
+
+    Task<T> Deserialize<T>(Stream stream, JsonSerializerOptions serializerOptions, CancellationToken cancellationToken);
 }
