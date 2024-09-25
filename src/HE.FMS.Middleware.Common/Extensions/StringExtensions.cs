8f661a80af4d64360df9e7517da61b257095eb83
mpgradzki <marcin.gradzki@pwc.com>
2024-09-25 16:27:19 +0200

    AB#108311 Implement logic for description field (#87)

--- src/HE.FMS.Middleware.Common/Extensions/StringExtensions.cs
+++ src/HE.FMS.Middleware.Common/Extensions/StringExtensions.cs
@@ -22,27 +22,4 @@ public static class StringExtensions
             ? value[..maxLength] + truncationSuffix
             : value;
     }
-
-    public static string? TrimEnd(this string? value, int trimLength)
-    {
-        return value?.Length > trimLength
-            ? value[..^trimLength]
-            : value;
-    }
-
-    public static string ReplaceAt(this string value, int index, int length, string replace)
-    {
-        return value
-            .Remove(index, Math.Min(length, value.Length - index))
-            .Insert(index, replace);
-    }
-
-    public static string ReplaceAt(this string value, int index, int length, string replace, char gapFiller)
-    {
-#pragma warning disable IDE0057 // Use range operator
-        return value
-            .Remove(index, length)
-            .Insert(index, replace.PadRight(length).Substring(0, length));
-#pragma warning restore IDE0057 // Use range operator
-    }
 }
