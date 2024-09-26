e71eeff1043eca4063b66bd50ae96ba22997c13b
Marcin Gradzki <marcin.gradzki@pwc.com>
2024-09-26 09:39:47 +0200

    improve vat code handling

--- src/HE.FMS.Middleware.Contract/Enums/VatCode.cs
+++ src/HE.FMS.Middleware.Contract/Enums/VatCode.cs
@@ -5,16 +5,16 @@ namespace HE.FMS.Middleware.Contract.Enums;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum VatCode
 {
-    VatCode02,
-    VatCode03,
-    VatCode04,
-    VatCode10,
-    VatCode11,
-    VatCode12,
-    VatCode13,
-    VatCode20,
-    VatCode21,
-    VatCode22,
-    VatCode98,
+    VatCode02 = 2,
+    VatCode03 = 3,
+    VatCode04 = 4,
+    VatCode10 = 10,
+    VatCode11 = 11,
+    VatCode12 = 12,
+    VatCode13 = 13,
+    VatCode20 = 20,
+    VatCode21 = 21,
+    VatCode22 = 22,
+    VatCode98 = 98,
 }
 #pragma warning restore CA1712 // Do not prefix enum values with type name
