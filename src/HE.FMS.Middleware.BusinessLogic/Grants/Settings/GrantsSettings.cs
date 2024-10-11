5013d1603892372c34415b4bbd003ba1b2154646
Marcin Gradzki <marcin.gradzki@pwc.com>
2024-10-11 15:49:50 +0200

    rewrite tests for classes that requires memory cache

--- src/HE.FMS.Middleware.BusinessLogic/Grants/Settings/GrantsSettings.cs
+++ src/HE.FMS.Middleware.BusinessLogic/Grants/Settings/GrantsSettings.cs
@@ -1,5 +1,8 @@
-﻿namespace HE.FMS.Middleware.BusinessLogic.Grants.Settings;
+using System.Diagnostics.CodeAnalysis;
 
+namespace HE.FMS.Middleware.BusinessLogic.Grants.Settings;
+
+[ExcludeFromCodeCoverage]
 internal sealed class GrantsSettings : IGrantsSettings
 {
     public string BranchId { get; set; }
